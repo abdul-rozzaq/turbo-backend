56f3d4b0ce7c218dfc407eea00016a5428816879
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2023-12-25 21:36:36 +0500

    xcczxcz

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'api',
     'shop',
 ]
@@ -85,7 +85,19 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
+
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
