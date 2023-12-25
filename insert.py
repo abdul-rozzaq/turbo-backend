4c1dab9399b773436f9ec96c1d26c256314974a2
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2023-12-25 21:28:30 +0500

    xxxxxxxxxxx

--- insert.py
+++ insert.py
@@ -8,35 +8,26 @@ django.setup()
 from shop.models import Product, Shop
 from faker import Faker
 
+import random
 
 fake = Faker()
 shops = Shop.objects.all()
 
 
-# for _ in range(5):
-#     Shop.objects.create(
-#         name=fake.word(),
-#         password=fake.word()
-#     )
+for _ in range(10):
+    Shop.objects.create(
+        name=fake.word(),
+        password=fake.word()
+    )
 
 
-# for shop in Shop.objects.all():
-#     for _ in range(10):
-#         Product.objects.create(
-#             shop=shop,
-#             name=fake.word(),
-#             price=fake.random_number(),
-#             count=fake.random_number(),
-#             type=fake.random_element(elements=('1', '4', '2', '3'))
-#         )
+for shop in Shop.objects.all():
+    for _ in range(10):
+        Product.objects.create(
+            shop=shop,
+            name=fake.word(),
+            price=random.randint(15, 150),
+            count=random.randint(20, 100) * 1000,
+            type=fake.random_element(elements=('1', '4', '2', '3'))
+        )
 
-import random
-
-for pr in Product.objects.all():
-    
-    pr.count = random.randint(15, 150)
-    pr.price = random.randint(20, 100) * 1000
-    
-    
-    
-    pr.save()
