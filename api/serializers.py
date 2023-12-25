e49378c61f456efe951a4c101894f51a35de39af
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2023-12-25 21:52:07 +0500

    cccccccccc

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,8 @@
 from django.forms import ValidationError
 from rest_framework import serializers
 
+from shop.models import Product
+
 
 from .models import Shop, Token
 
@@ -11,28 +13,24 @@ class ShopLoginSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=256)
     password = serializers.CharField(max_length=256)
     device = serializers.CharField(max_length=256, default='None')
-    
+
     def validate(self, attrs):
         val = super().validate(attrs)
-        
+
         name = attrs.get('name')
         password = attrs.get('password')
-                        
+
         if not Shop.objects.filter(name=name, password=password).exists():
             raise ValidationError('Shop not found')
-            
+
         return val
-    
-    
+
     def to_representation(self, instance):
         return super().to_representation(instance)
-    
-    
 
 
 class ShopSerializer(serializers.ModelSerializer):
-    
-    
+
     class Meta:
         model = Shop
         exclude = ['password']
@@ -42,3 +40,16 @@ class TokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Token
         fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+
+        data['type'] = instance.get_type_display()
+
+        return data
--- api/views.py
+++ api/views.py
@@ -5,30 +5,37 @@ import json
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.response import Response
 
+from shop.models import Product
+
 
 from .authentication import TokenAuthentication
 from .models import Token, Shop
-from .serializers import ShopLoginSerializer, ShopSerializer, TokenSerializer
+from .serializers import ProductSerializer, ShopLoginSerializer, ShopSerializer, TokenSerializer
+
 
 @api_view(['POST'])
 def login(request):
-    
+
     setializer = ShopLoginSerializer(data=request.POST)
 
     if setializer.is_valid():
-        shop = Shop.objects.get(name=setializer.data['name'], password=setializer.data['password'])
-        token = Token.objects.create(shop=shop, device=setializer.data['device'])
-        
+        shop = Shop.objects.get(
+            name=setializer.data['name'], password=setializer.data['password'])
+        token = Token.objects.create(
+            shop=shop, device=setializer.data['device'])
+
         return Response({'token': token.key, 'shop': ShopSerializer(shop).data}, status=200)
     else:
         return Response(setializer.errors, status=400)
-    
+
 
 @api_view(['GET'])
 @authentication_classes([TokenAuthentication])
 def products(request):
-    return Response(status=200)
-
+    products = Product.objects.filter(shop=request.user)
+    serializer = ProductSerializer(products, many=True)
+    
+    return Response(serializer.data, status=200)
 
 
 @api_view(['GET'])
@@ -37,9 +44,10 @@ def get_tokens(request, shop_id):
     serializer = TokenSerializer(tokens, many=True)
     return Response(serializer.data, status=200)
 
+
 @api_view(['DELETE'])
 def delete_token(request, token_id):
-    
-    Token.objects.get(key=token_id).delete();
-    
+
+    Token.objects.get(key=token_id).delete()
+
     return Response(status=204)
